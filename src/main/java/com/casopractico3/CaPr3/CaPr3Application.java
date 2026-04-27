e6411e6789a9887fee9bf632c85214ba30a3b811
javierGP <jgarcpor@emeal.nttdada.com>
2026-04-27 09:11:30 +0200

    Primer commit

---
+++ src/main/java/com/casopractico3/CaPr3/CaPr3Application.java
@@ -0,0 +1,13 @@
+package com.casopractico3.CaPr3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaPr3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaPr3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/casopractico3/CaPr3/CaPr3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casopractico3.CaPr3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaPr3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
